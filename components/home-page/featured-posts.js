bdf08a4b4c820bec36654afa9ccb4bf936631267
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-05 09:00:43 +0200

    feat(app): added global styles

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,7 @@
+import classes from "./featured-posts.module.css";
+
+function FeaturedPosts() {
+  return <div className={classes.latest}>Featured Posts Component</div>;
+}
+
+export default FeaturedPosts;
---
+++ components/home-page/hero.js
@@ -0,0 +1,7 @@
+import classes from "./hero.module.css";
+
+function Hero() {
+  return <div className={classes.hero}>Hero Component</div>;
+}
+
+export default Hero;
