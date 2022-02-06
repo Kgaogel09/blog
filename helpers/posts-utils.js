a7aea32824b6413194eca33ee7b63dff836df362
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-07 01:11:46 +0200

    refactor(utils): create functions to read and fetch data from markdown

---
+++ helpers/posts-utils.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+function getPostData(fileNAme) {
+  const filePath = path.join(postsDirectory, fileNAme);
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(fileContent);
+
+  const postSlug = fileNAme.replace(/\.md/, ""); //removes file extension
+
+  const postData = {
+    slug: postSlug,
+    ...data,
+    content,
+  };
+
+  return postData;
+}
+
+export function getALlPost() {
+  const postFiles = fs.readdirSync(postsDirectory);
+
+  const allPosts = postFiles.map((postFile) => {
+    return getPostData(postFile);
+  });
+
+  allPosts.sort((postA, postB) => (postA.date > postB.date ? -1 : 1));
+
+  return sortedPosts;
+}
+
+export function getFeaturedPosts() {
+  const allPost = getALlPost();
+
+  const featuredPosts = allPost.filter((post) => post.isFeatured);
+
+  return featuredPosts;
+}
