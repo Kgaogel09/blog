3606554ddbb596ee5ee04d03125512c7c1c710cb
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-07 23:53:34 +0200

    refactor(contact): store messages with mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -20,8 +22,30 @@ function handler(req, res) {
       message,
     };
 
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://thorn:IRI0MuXdi2kEBUaE@cluster0.naxcy.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect too the database!" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const results = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = results.insertId;
+    } catch (error) {
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
+
     res.status(201).json({ message: "Success!", message: newMessage });
-    console.log(newMessage);
   }
 }
 
